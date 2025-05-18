3cd298ab341e00e51d278d75775b09af52df1ccb
EmilioRXD <jes99.444.rodriguez@gmail.com>
2025-05-18 03:26:48 -0400

    Corrección error Variable Autenticación

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -17,7 +17,7 @@ load_dotenv()
 # Configuración de JWT
 SECRET_KEY = os.getenv("JWT_SECRET")
 ALGORITHM = os.getenv("JWT_ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES"))
+JWT_EXPIRATION_MINUTES = os.getenv("JWT_EXPIRATION_MINUTES")
 
 # Configuración de hash de contraseñas
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -46,7 +46,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=JWT_EXPIRATION_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
